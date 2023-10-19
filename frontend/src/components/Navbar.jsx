2fffd21209ddb1a3a9a15a6f2b41f1388fbc2243
Bastián Núñez <bastinuez@hotmail.com>
2023-10-19 00:26:34 -0300

    feat: base hecha para crear las vistas necesarias

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import { Link, Outlet, useLocation, useNavigate} from 'react-router-dom';
 import {useAuth} from '../context/AuthContext'
 
+
+
 export const NavBarExport = () => {
-	const { state } = useLocation();
+	const {showSidebar,setShowSidebar,authUser, updateAuth} = useAuth()
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShowSidebar(false);
+	const handleShow = () => setShowSidebar(true);
+
+	
+	
 	const navigate = useNavigate();
 
-	const {authUser, updateAuth} = useAuth() 
 	var pathRol="/"
 	const onLogout = () => {
 		updateAuth('')
@@ -23,21 +31,31 @@ export const NavBarExport = () => {
 		pathRol="/"+authUser.rol.toLowerCase()
 	}
 	
+	
 	return (
 		<>
 			<header className='w-100'>
 				<Navbar className="bg-body-secondary">
-					<Container>
+					<Container className='w-100' style={{maxWidth:"none"}}>
+						{
+							authUser ? 
+							<Button variant="primary me-5" className="" onClick={handleShow}>
+								<i className="bi bi-list" ></i>
+								Menu
+							</Button>
+							:
+							<></>
+						}
+						
 						<Navbar.Brand href={pathRol}>LOGO</Navbar.Brand>
 						<Navbar.Toggle></Navbar.Toggle>
 						<Navbar.Collapse className="justify-content-end">
-							
 							<div>
 								{authUser ? (
 									<div className='user d-flex'>
 										<div className='ms-3 me-3 align-items-center d-flex'>
 											<Navbar.Text>
-												<span className='username'>{authUser.nombres}</span>
+												<span className='username'>Usuario: {authUser.nombres}</span>
 											</Navbar.Text>
 										</div>
 										<div className='ms-3 me-3'>
